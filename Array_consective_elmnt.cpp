8913dffd7a28ace47aa08e48b786c26d4b678029
Ravibabuc <42927631+Ravibabuc@users.noreply.github.com>
2021-12-30 07:12:42 +0530

    Create Array_consective_elmnt.cpp
    roblem Description
    Given an array of positive integers A, check and return whether the array elements are consecutive or not.
    
    NOTE: Try this with O(1) extra space.
    
    
    
    Problem Constraints
    
    1 <= length of the array <= 100000
    1 <= A[i] <= 10^9
    
    
    Input Format
    
    The only argument given is the integer array A.
    
    
    Output Format
    
    Return 1 if the array elements are consecutive else return 0.
    
    
    Example Input
    
    Input 1:
    
     A = [3, 2, 1, 4, 5]
    
    Input 2:
    
     A = [1, 3, 2, 5]
    
    
    
    Example Output
    
    Output 1:
    
     1
    
    Output 2:
    
     0
    
    
    
    Example Explanation
    
    Explanation 1:
    
     As you can see all the elements are consecutive, so we return 1.
    
    Explanation 2:
    
     Element 4 is missing, so we return 0.

---
+++ Array_consective_elmnt.cpp
@@ -0,0 +1,97 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <set>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//struct Data
+//{
+ //   int Task;
+  //  int Priority;
+//};
+
+int solve(vector<int> &A) 
+{
+    int n =A.size();
+    sort(A.begin(), A.end());
+    
+    
+    for(int i =1;i<n;i++)
+    {
+        if(A[i]  == (A[i-1] +1))
+            continue;
+        else return 0;
+    }
+    return 1;
+}
+
+
+int solve1(vector<int> &A, vector<int> &B)
+{
+    //Data Node;
+    int n =A.size();
+   
+   // int Order=1;
+    int clk=0;
+    queue<int> Que;
+    
+    
+    for(int i =0;i<n;i++)
+    {
+      // Node.Task=A[i];
+       // Node.Priority=B[i];
+        Que.push(A[i]);
+    }
+    int i=0;
+    while(!Que.empty())
+    {
+        //cout<<"Que "<<Que.front()<<endl;
+      //  cout<<"Que "<<Que.front().Priority<<endl;
+        if(Que.front() == B[i])
+        {
+            Que.pop();
+            i ++;
+            
+        }
+        else
+        {
+            Que.push(Que.front());
+            Que.pop();
+        }
+        clk++;
+    }
+    
+    return clk;
+}
+
+
+
+
+
+int main()
+{
+  // vector<int> A ={2, 3, 1, 5, 4};
+ // vector<int> B ={1, 3, 5, 4, 2};
+   
+   vector<int> A ={3, 2, 1, 4, 5 };
+ //  vector<int> B ={3, 7, 2, 5, 1, 8, 4, 6};
+   
+   int ans;
+   int n =A.size();
+   ans = solve(A);
+
+    std::cout <<" ans " <<ans<< std::endl;
+
+
+    return 0;
+}
+
